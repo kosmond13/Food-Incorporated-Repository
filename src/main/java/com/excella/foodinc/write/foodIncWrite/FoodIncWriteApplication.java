c2b3414210187f65dbabeeeda00114c206989a4d
alex griffith <alex.griffith@excella.com>
2019-02-19 11:08:44 -0500

    first commit

---
+++ src/main/java/com/excella/foodinc/write/foodIncWrite/FoodIncWriteApplication.java
@@ -0,0 +1,13 @@
+package com.excella.foodinc.write.foodIncWrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodIncWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodIncWriteApplication.class, args);
+	}
+
+}
